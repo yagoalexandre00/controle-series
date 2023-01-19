1b3db702ecdf39b7859bb827bc74ef009891e39b
Yago Alexandre <goltarayago@gmail.com>
2023-01-19 13:00:29 -0300

    estilizando layout do header-container

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 
     <div class="header-container">
         <h1>Página inicial</h1>
+        <p>Seja bem-vindo(a) ao App de controle de Séries e Filmes!</p>
+        <p>Aqui você poderá listar as séries e/ou filmes que está assistindo no momento ou pretende assistir.</p>
     </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
